5abec57c39fe9160d0b6a4a40274f6c87c4561f1
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-03-12 14:43:12 -0400

    Delete metadata.h

--- metadata.h
+++
@@ -1,9 +0,0 @@
-#ifndef METADATA_H
-#define METADATA_H
-
-#include <string>
-
-std::string get_metadata(const std::string &filename);
-void set_metadata(const std::string &filename, const std::string &key, const std::string &value);
-
-#endifv
